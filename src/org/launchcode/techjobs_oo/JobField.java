8efb59a35792b0009ac1ce17917367681a4a6c84
Lb-LC101 <lisabartfeld@gmail.com>
2021-01-30 23:09:26 -0600

    created base class JobField, extended Employer and Location classes

---
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -0,0 +1,46 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    //constructors
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    //methods
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    //getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
